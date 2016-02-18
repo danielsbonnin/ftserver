f0e9b6b537c6d4edb08b978f8f4a28f40a70b5f3
danielsbonnin <bonnind@oregonstate.edu>
2016-02-18 17:03:08 -0600

    Started ftserver argument handling

--- ftserver.cpp
+++ ftserver.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
+int main(int argc, char** argv) {
+	for (int i = 0; i < argc; i++) {
+		cout << argv[i];
+	}
 	string letter = "";
 	cout << "Hello";
 	getchar();
