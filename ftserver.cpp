4fee859dec777f93ccac34ae2ae1e347e01c2bb4
danielsbonnin <bonnind@oregonstate.edu>
2016-02-19 09:47:15 -0600

    ftserver handles port argument

--- ftserver.cpp
+++ ftserver.cpp
@@ -1,13 +1,51 @@
 #include <iostream>
-
+#include "ftserver.h"
+#include <string>
+#include <stdexcept>
 using namespace std;
 
+
+
 int main(int argc, char** argv) {
-	for (int i = 0; i < argc; i++) {
-		cout << argv[i];
-	}
-	string letter = "";
-	cout << "Hello";
+	int portno = getPort(argc, argv);
+	if (portno == -1)  //Invalid command line port input
+		return 0;  //quit gracefully.
+
+	
+	cout << "You entered port number: " << portno << "\n";
+
+
 	getchar();
 	return 0;
+}
+
+int getPort(int argc, char **argv) {
+	int portno = 0;
+
+	if (argc != 2) { //Invalid num args.
+		cout << "Invalid Command Line Arguments\n" << USAGE;
+		// TODO: remove this for linux.
+		getchar();
+		return -1;
+	}
+
+	/* Help for exception handling: 
+	 * http://www.cplusplus.com/reference/stdexcept/invalid_argument/
+	 */
+	try {
+		portno = stoi(argv[1]);
+	}
+	catch (const invalid_argument &e) // non-integer entered.
+	{
+		cout << "Please enter an integer port argument.\n" << USAGE;
+		getchar(); //TODO: remove this for linux.
+		return -1;
+	}
+	if (portno < PORT_MIN || portno > PORT_MAX) {
+		cout << "Port Number argument out of range\n" << USAGE;
+		getchar();  //TODO: remove this for linux.
+		return -1;
+	}
+	else
+		return portno;
 }
--- ftserver.h
+++ ftserver.h
@@ -1,4 +1,15 @@
 #ifndef FTSERVER_H
 #define FTSERVER_H
 
+/*
+ * Process commandline port argument.
+ * @param argc number of commandline arguments
+ * @param argv array of argument strings
+ * @return valid port number or -1 on invalid
+ */
+
+#define PORT_MAX 65535
+#define PORT_MIN 1024
+#define USAGE "Usage: ./ftserver <int PORTNO (1024 - 65535)>\n"
+int getPort(int argc, char** argv);
 #endif
