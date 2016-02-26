eb44945edb7fb5007fa190f051cd14a80b3457fd
danielsbonnin <bonnind@oregonstate.edu>
2016-02-26 16:45:48 -0600

    ftclient is saving files in windows

--- ftclient.py
+++ ftclient.py
@@ -2,19 +2,40 @@
 #^
 import argparse
 import socket
+import socketserver
+import re
+
+#Command identifiers. Go inside html style <\> tags to be sent to server. 
+GET_COMMAND = "g"
+LIST_COMMAND = "l"
+OK_TAG = "ok"
+FILE_TAG = "file"
+ERROR_TAG = "error"
+LIST_TAG = "list"
+ITEM_TAG = "item"
+NAME_TAG = "name"
+DATA_TAG = "data"
 
 def main():
     args = cliHandler()
 
-    #Create a tcp socket. 
-    s = socket.socket(
+    #Create 2 sockets: control and data. 
+    cntrl = socket.socket(
+        socket.AF_INET, socket.SOCK_STREAM)
+    dataSocket = socket.socket(
         socket.AF_INET, socket.SOCK_STREAM)
 
     #Verify Server address and contact server
-    initContact(args, s)
-    sendCommand(s)
-    s.close
-    print("socket is closed()")
+    initContact(args, cntrl)
+    commandMSG = parseCommand(args)
+    sendCommand(cntrl, commandMSG)
+    data = receiveData(dataSocket, args)
+    cntrl.close()
+    dataSocket.close()
+    parseServerData(data)
+
+    
+    
     input() #TODO: fix raw_input/input issue
     exit(0)
 
@@ -65,11 +86,10 @@ def initContact(args, s):
         s.close()
         exit(1)
 
-def sendCommand(s):
+def sendCommand(s, commandMSG):
     #@param s the socket object
-    
     try:  #format and send command
-        s.sendall(("Test client-server").encode('utf-8'))
+        s.sendall((commandMSG).encode('utf-8'))
     except socket.error as e:
         #close program if error sending
         if (e.errno == errno.ECONNRESET or e.errno == errno.ECONNABORTED):
@@ -78,7 +98,64 @@ def sendCommand(s):
             exit(1)
         else:
             s.close()
+            print("Socket Exception")
             exit(1)
 
-if __name__ == '__main__':
+def receiveData(dataSocket, args):
+    dataSocket.bind(('', int(args.DATA_PORT)))
+    dataSocket.listen(1)
+    data, serverAddr = dataSocket.accept()
+    return data.recv(1024).decode('utf-8')
+
+def parseServerData(serverMessage):
+    if OK_TAG in serverMessage:
+        if LIST_TAG in serverMessage:
+            printList(serverMessage)
+        elif NAME_TAG in serverMessage:
+            saveFile(serverMessage)
+    else:
+        if ERROR_TAG in serverMessage:
+            printError(serverMessage);
+        else:
+            print("Server Message is in an unrecognized format" + serverMessage)  
+
+def printList(serverMessage):
+    filenames = parseTag(ITEM_TAG, serverMessage)
+    print("***Files in remote directory***")
+    for i in filenames:
+        print(i)
+
+def saveFile(serverMessage):
+    filename = parseTag(NAME_TAG, serverMessage)
+    data = parseTag(DATA_TAG, serverMessage)
+    newFile = open(filename[0], 'w+')
+    newFile.write(data[0])
+    newFile.close()
+    
+    print("contents of new file: " + filename[0] + ": ")
+    newFile = open(filename[0], 'r')
+    contents = newFile.read(50)
+    print(contents)
+    newFile.close()
+
+def printError(serverMessage):
+    errorMSG = parseTag(ERROR_TAG, serverMessage)
+    print(errorMSG[0])
+
+def parseCommand(args):
+    if (args.l):
+        command = '<' + LIST_COMMAND + '>'
+    else:
+        command = '<' + GET_COMMAND + '>' + args.g + '</' + GET_COMMAND + '>'
+    return command + '<dataport>' + str(args.DATA_PORT) + '</dataport>'
+
+def parseTag(tag, msg):
+    openTag = '<' + tag + '>'
+    closeTag = '</' + tag + '>'
+    if openTag not in msg or closeTag not in msg:
+        return None
+    else:
+        return re.findall(openTag + '(.*?)' + closeTag, msg)
+    
+if (__name__ == '__main__'):
     main()
